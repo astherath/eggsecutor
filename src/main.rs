0247ab7e67a9944888d65c60a29f28f7ea742757
astherath <farceriv@gmail.com>
2021-11-21 21:54:35 -0500

    moved file io out into its own file

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use serde::{Deserialize, Serialize};
 use std::io;
 use std::process::{Command, Stdio};
 mod errors;
+mod file_io;
 mod output_display;
 mod subcommands;
 
@@ -237,64 +238,5 @@ fn is_process_alive(pid: &str) -> io::Result<bool> {
     {
         Some(code) => Ok(code == 0),
         None => Ok(false),
-    }
-}
-
-mod file_io {
-    use super::errors;
-    use super::ProcessInfo;
-    use clap;
-    use std::env;
-    use std::fs::{self, File};
-    use std::io;
-    use std::path::Path;
-
-    // TODO: this is not a good cross dependency; find fix.
-    use super::is_process_alive;
-
-    pub fn write_processes_to_state_file(processes: Vec<ProcessInfo>) -> io::Result<()> {
-        let state_file_path = get_state_file_path();
-        let updated_processes = serde_json::to_string(&processes)?;
-        fs::write(state_file_path, updated_processes.as_bytes())?;
-
-        Ok(())
-    }
-
-    pub fn get_processes_from_state_file() -> io::Result<Vec<ProcessInfo>> {
-        let state_file_path = get_state_file_path();
-        let contents = fs::read_to_string(state_file_path)?;
-        let mut processes: Vec<ProcessInfo> = serde_json::from_str(&contents)?;
-        processes.retain(|process| is_process_alive(&process.pid).unwrap());
-        Ok(processes)
-    }
-
-    pub fn check_if_file_is_valid(filename: &str) -> Result<(), clap::Error> {
-        match Path::new(filename).exists() {
-            true => Ok(()),
-            false => Err(errors::get_invalid_file_path_error()),
-        }
-    }
-
-    pub fn create_state_file_if_not_exists() -> io::Result<()> {
-        let state_file_path = get_state_file_path();
-        if !Path::new(&state_file_path).exists() {
-            File::create(&state_file_path)?;
-        }
-        Ok(())
-    }
-
-    fn get_state_file_path() -> String {
-        let path_string = match env::var("EGGSECUTOR_STATE_FILE") {
-            Ok(state_path) => state_path,
-            Err(_) => "~/.eggsecutor.state".to_string(),
-        };
-
-        shellexpand::tilde(&path_string).to_string()
-    }
-
-    #[cfg(test)]
-    mod tests {
-        #[test]
-        fn pass() {}
     }
 }
